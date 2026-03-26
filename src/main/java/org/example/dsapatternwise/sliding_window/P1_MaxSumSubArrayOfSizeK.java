a90095b844915e07873a324d1e0563e0c68db8fe
mohnish1904 <mohnish190497@gmail.com>
2026-03-26 23:17:15 +0530

    added info

--- src/main/java/org/example/dsapatternwise/sliding_window/P1_MaxSumSubArrayOfSizeK.java
+++ src/main/java/org/example/dsapatternwise/sliding_window/P1_MaxSumSubArrayOfSizeK.java
@@ -3,7 +3,15 @@ package org.example.dsapatternwise.sliding_window;
 public class P1_MaxSumSubArrayOfSizeK {
 
     /**
+     *Given an array of integers arr[]  and a number k. Return the maximum sum of a subarray of size k.
      *
+     * Note: A subarray is a contiguous part of any given array.
+     *
+     * Examples:
+     *
+     * Input: arr[] = [100, 200, 300, 400], k = 2
+     * Output: 700
+     * Explanation: arr2 + arr3 = 700, which is maximum.
      */
     public static void main(String[] args) {
         int[] newArr = new int[]{100, 200, 300, 400, 500};
