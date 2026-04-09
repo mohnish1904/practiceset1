dfb4c894e6460edfc3c287d3814cd56108eb9501
mohnish1904 <mohnish190497@gmail.com>
2026-04-09 11:03:51 +0530

    153. Find Minimum in Rotated Sorted Array

---
+++ src/main/java/org/example/dsapatternwise/binarysearch/P5_MinElementInRotatedArr.java
@@ -0,0 +1,68 @@
+package org.example.dsapatternwise.binarysearch;
+
+public class P5_MinElementInRotatedArr {
+
+    /**
+     * 153. Find Minimum in Rotated Sorted Array
+     * Medium
+     * Suppose an array of length n sorted in ascending order is rotated between 1 and n times.
+     * For example, the array nums = [0,1,2,4,5,6,7] might become:
+     * [4,5,6,7,0,1,2] if it was rotated 4 times.
+     * [0,1,2,4,5,6,7] if it was rotated 7 times.
+     * Notice that rotating an array [a[0], a[1], a[2], ..., a[n-1]] 1 time results in the array [a[n-1],
+     * a[0], a[1], a[2], ..., a[n-2]].
+     * Given the sorted rotated array nums of unique elements, return the minimum element of this array.
+     * You must write an algorithm that runs in O(log n) time.
+     * <p>
+     * Example 1:
+     * Input: nums = [3,4,5,1,2]
+     * Output: 1
+     * Explanation: The original array was [1,2,3,4,5] rotated 3 times.
+     * Example 2:
+     * Input: nums = [4,5,6,7,0,1,2]
+     * Output: 0
+     * Explanation: The original array was [0,1,2,4,5,6,7] and it was rotated 4 times.
+     * Example 3:
+     * Input: nums = [11,13,15,17]
+     * Output: 11
+     * Explanation: The original array was [11,13,15,17] and it was rotated 4 times.
+     * <p>
+     * Constraints:
+     * n == nums.length
+     * 1 <= n <= 5000
+     * -5000 <= nums[i] <= 5000
+     * All the integers of nums are unique.
+     * nums is sorted and rotated between 1 and n times.
+     */
+
+    public static void main(String[] args) {
+        int[] arr = {11, 13, 15, 17};
+        System.out.println(minElement(arr));
+    }
+
+    private static int minElement(int[] nums) {
+
+        // break the nums into 2 parts, where the arr got rotated
+        // 1st part -> all the elements which are less than the last element of arr
+        // 2nd part -> all the elements which are greater than last element of arr
+
+        // p2 : 4,5,6,7,    p1 : 0,1,2
+
+        // use binary search to find first occurrence of p1
+
+        int low = 0, high = nums.length - 1, lstIdx = nums.length - 1, min = nums[0];
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (nums[mid] > nums[lstIdx]) {
+                low = mid + 1;
+            } else {
+                min = nums[mid];
+                high = mid - 1;
+            }
+        }
+
+        return min;
+    }
+}
